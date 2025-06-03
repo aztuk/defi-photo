f0f7cbb48438a42bcf9d7979cd0f76d8ca37266c
aztuk <48598254+aztuk@users.noreply.github.com>
2025-06-03 12:20:35 +0200

    Password and admin fix for mobile

--- src/app/features/admin/admin-page.component.ts
+++ src/app/features/admin/admin-page.component.ts
@@ -2,18 +2,36 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MissionFormComponent } from './mission-form/mission-form.component';
 import { MissionByPlanetComponent } from "./mission-by-planet/mission-by-planet.component";
+import { environment } from '../../../environments/environments';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [CommonModule, MissionFormComponent, MissionByPlanetComponent],
+  imports: [CommonModule, MissionFormComponent, MissionByPlanetComponent,FormsModule],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss',
 })
 export class AdminPageComponent {
   readonly showPanel = signal(false);
+  readonly isAuthenticated = signal<boolean>(
+    localStorage.getItem('admin-auth') === 'ok'
+  );
+  passwordInput = '';
+  error = '';
 
   constructor() {
+      this.isAuthenticated.set(false);
+  }
+
+  checkPassword() {
+    if (this.passwordInput === environment.adminPassword) {
+      localStorage.setItem('admin-auth', 'ok');
+      this.isAuthenticated.set(true);
+    } else {
+      this.error = 'Mot de passe incorrect.';
+      this.passwordInput = '';
+    }
   }
 
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  adminPassword: 'wedding2025',
   production: false,
   supabaseUrl: 'https://parzltsfnayrsaoptnrl.supabase.co',   // Ton URL Supabase
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhcnpsdHNmbmF5cnNhb3B0bnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NTYzMTYsImV4cCI6MjA2NDQzMjMxNn0.6c9D7pMO2FLae7WPP3wdGQikk44VW5OGQGLTEWR_Zlc', // Ta clé publique
